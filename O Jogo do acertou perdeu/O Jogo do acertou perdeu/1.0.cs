6df360da8122d889a72ddf6efa5b4ea8905d34d2
Eriel-Jesus <83691312+Eriel-Jesus@users.noreply.github.com>
2025-06-11 22:26:06 -0300

    Atualizações do jogo
    Atualizações constantes serão feitas, com novas versões do jogo. Melhorias pequenas, mas efetivas, com salvamento recorrente para evitar problemas.

---
+++ O Jogo do acertou perdeu/O Jogo do acertou perdeu/1.0.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace O_Jogo_do_acertou_perdeu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int menor = 1;
+            int maior = 100;
+            int palpite, oculto;
+
+            do
+            {
+                Console.Write("Insira o número oculto (entre 1 e 100): ");
+                oculto = int.Parse(Console.ReadLine());
+            } while (oculto <= menor || oculto >= maior);
+
+            for (int jogador = 1; jogador <= 2; jogador++)
+            {
+                do
+                {
+                    Console.Write("Jogador {0}, insira o seu palpite (entre {1} e {2}): ", jogador, menor, maior);
+                    palpite = int.Parse(Console.ReadLine());
+                }
+                while (palpite <= menor || palpite >= maior);
+
+                if (palpite == oculto)
+                {
+                    Console.WriteLine("Parabéns, jogador {0}, você perdeu!!!", jogador);
+                    jogador = 3;
+                }
+                else
+                {
+                    if(palpite > oculto)
+                    {
+                        maior = palpite;
+                    }
+                    else
+                    {
+                        menor = palpite;
+                    }
+
+                    if (jogador == 2)
+                    {
+                        jogador = 0;
+                    }
+                }
+                
+
+            }
+
+
+        }
+    }
+}
---
+++ O Jogo do acertou perdeu/O jogo do acertou perdeu 2.0/2.0.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace O_Jogo_do_acertou_perdeu
+{
+    internal class doispontozero
+    {
+        static void Main(string[] args)
+        {
+            int menor = 1;
+            int maior = 100;
+            int palpite, oculto;
+            bool verifica;
+
+            
+            
+                Console.Write("Insira o número oculto (entre 1 e 100): ");
+                verifica = int.TryParse(Console.ReadLine(), out oculto);
+
+            while ((oculto <= menor || oculto >= maior) || verifica == false)
+            {
+                Console.Clear();
+                Console.Write("Coloca o número certo, seu energúmeno (entre 1 e 100): ");
+                verifica = int.TryParse(Console.ReadLine(), out oculto);
+            }
+
+            for (int jogador = 1; jogador <= 2; jogador++)
+            {
+                do
+                {
+                    Console.Write("Jogador {0}, insira o seu palpite (entre {1} e {2}): ", jogador, menor, maior);
+                    verifica = int.TryParse(Console.ReadLine(), out palpite);
+                }
+                while ((palpite <= menor || palpite >= maior) || verifica == false);
+
+                if (palpite == oculto)
+                {
+                    Console.WriteLine("Parabéns, jogador {0}, você perdeu!!!", jogador);
+                    jogador = 3;
+                }
+                else
+                {
+                    if (palpite > oculto)
+                    {
+                        maior = palpite;
+                    }
+                    else
+                    {
+                        menor = palpite;
+                    }
+
+                    if (jogador == 2)
+                    {
+                        jogador = 0;
+                    }
+                }
+
+
+            }
+
+
+        }
+    }
+}
+
